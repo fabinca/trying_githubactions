449ee204f78ba7607bbd01a3ce0d5d7b022dda44
cfabian <cfabian@student.42wolfsburg.de>
2022-05-16 14:50:16 +0200

    test

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cfabian <cfabian@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 14:41:02 by cfabian           #+#    #+#             */
-/*   Updated: 2022/05/16 14:45:04 by cfabian          ###   ########.fr       */
+/*   Updated: 2022/05/16 14:48:08 by cfabian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int argc, char **argv)
 	i = 0;
 	while (i < argc)
 	{
-		printf("arg %i is %s ..\n", i, argv[i]);
+		printf("arg %i is %s \n", i, argv[i]);
 		i++;
 	}
 	return (0);
