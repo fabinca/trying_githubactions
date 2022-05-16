c1a401469de2917bf802e3c207498c322711f792
cfabian <cfabian@student.42wolfsburg.de>
2022-05-16 15:03:04 +0200

    greating segfault

--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 /*   By: cfabian <cfabian@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 14:41:02 by cfabian           #+#    #+#             */
-/*   Updated: 2022/05/16 15:01:39 by cfabian          ###   ########.fr       */
+/*   Updated: 2022/05/16 15:02:58 by cfabian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int	main(int argc, char **argv)
 {
