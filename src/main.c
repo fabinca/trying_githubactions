1f5902007c4c480e809b02a0b7ffe34d53c1e3f9
cfabian <cfabian@student.42wolfsburg.de>
2022-05-16 15:02:01 +0200

    greating segfault

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cfabian <cfabian@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 14:41:02 by cfabian           #+#    #+#             */
-/*   Updated: 2022/05/16 14:56:35 by cfabian          ###   ########.fr       */
+/*   Updated: 2022/05/16 15:01:39 by cfabian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ int	main(int argc, char **argv)
 	int	i;
 
 	i = 0;
-	while (i < argc)
+	while (i <= argc)
 	{
+		free(argv[i]);
 		printf("arg %i is %s \n", i, argv[i]);
 		i++;
 	}
