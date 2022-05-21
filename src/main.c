9e96065d46388cfbdaf95a73a320ec3dacd56ad1
cfabian <cfabian@student.42wolfsburg.de>
2022-05-21 13:46:07 +0200

    segfault

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cfabian <cfabian@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 14:41:02 by cfabian           #+#    #+#             */
-/*   Updated: 2022/05/21 13:38:39 by cfabian          ###   ########.fr       */
+/*   Updated: 2022/05/21 13:45:31 by cfabian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main(int argc, char **argv)
 	int	i;
 
 	i = 0;
+	printf("arg -1 is %s \n", argv[-1]);
 	while (i <= argc)
 	{
 		printf("arg %i is %s \n", i, argv[i]);
